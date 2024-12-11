8beee3f855220e491abe5ac1f94daf585fabfa91
SagarLonkar-18 <sagarlonkar75@gmail.com>
2024-12-11 12:09:15 +0530

    loaded textures

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 import * as THREE from "three"
-// import { RGBELoader } from "three/examples/jsm/Addons.js";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
 import gsap from "gsap";
 
-// Set up the camera 
+// Setting up the camera 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(25,window.innerWidth/window.innerHeight,0.1,100);
 camera.position.z = 9;
 
+// Setting up the lights and hdri 
+const loader = new RGBELoader();
+loader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/moonlit_golf_1k.hdr',function(texture){
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
+});
+
+const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(5,5,5);
+scene.add(directionalLight);
+
+
+// Setting up the planets
 const radius = 1.3;
 const segments = 64;
-const colors = ["red", "green", "blue", "yellow"]
 const orbitRadius = 4.5;
+const colors = ["red", "green", "blue", "yellow"];
+const textures = ["./csilla/color.png","./earth/map.jpg","./venus/map.jpg","./volcanic/color.png"];
 const spheres = new THREE.Group();
 
 // ------- POSITIONING FOR 4 PLANETS --------------------------------
@@ -28,9 +45,14 @@ const spheres = new THREE.Group();
 // Postioning of Z = r*sin(theta)    <-----
 
 for(let i=0; i<4; i++) {
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load(textures[i]);
+
   const geometry = new THREE.SphereGeometry(radius,segments,segments);
-  const material = new THREE.MeshBasicMaterial({ color: colors[i] , antialias: true });
+  const material = new THREE.MeshStandardMaterial({ map:texture });
   const sphere = new THREE.Mesh(geometry, material);
+
+
   const angle = (i/4)*(Math.PI*2);
   sphere.position.x = orbitRadius * Math.cos(angle);
   sphere.position.z = orbitRadius * Math.sin(angle);  
@@ -41,7 +63,7 @@ spheres.rotation.x = 0.1;
 spheres.position.y = -0.8;
 scene.add(spheres);
 
-// Set up the renderer
+// Setting up the renderer
 const renderer = new THREE.WebGLRenderer({ canvas : document.querySelector("canvas"), antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
