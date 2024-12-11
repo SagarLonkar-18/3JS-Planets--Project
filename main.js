a63eb6c4576b146b5217c7554e2b5c60502a518a
SagarLonkar-18 <sagarlonkar75@gmail.com>
2024-12-11 21:37:00 +0530

    added headings and sync planets animation with headings

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 directionalLight.position.set(5,5,5);
 scene.add(directionalLight);
 
-
 // Setting up the planets
 const radius = 1.3;
 const segments = 64;
@@ -29,6 +28,19 @@ const orbitRadius = 4.5;
 const colors = ["red", "green", "blue", "yellow"];
 const textures = ["./csilla/color.png","./earth/map.jpg","./venus/map.jpg","./volcanic/color.png"];
 const spheres = new THREE.Group();
+
+// Making the big sphere to apply stars texture on its backside ( this sphere contains our planets)
+const starSphereTexture = new THREE.TextureLoader().load("./stars.jpg");
+starSphereTexture.colorSpace = THREE.SRGBColorSpace;
+const starGeometry = new THREE.SphereGeometry(50,64,64);
+const starMaterial = new THREE.MeshStandardMaterial({
+  map: starSphereTexture,
+  side: THREE.BackSide,
+  transparent:true,
+  opacity: 0.7,
+});
+const starSphere = new THREE.Mesh(starGeometry, starMaterial);
+scene.add(starSphere);
 
 // ------- POSITIONING FOR 4 PLANETS --------------------------------
 // Planet 1 at 0 degree : (0/4 ) * (Math.PI*2) = 0 * 360 = 0
@@ -47,11 +59,11 @@ const spheres = new THREE.Group();
 for(let i=0; i<4; i++) {
   const textureLoader = new THREE.TextureLoader();
   const texture = textureLoader.load(textures[i]);
+  texture.colorSpace = THREE.SRGBColorSpace;
 
   const geometry = new THREE.SphereGeometry(radius,segments,segments);
   const material = new THREE.MeshStandardMaterial({ map:texture });
   const sphere = new THREE.Mesh(geometry, material);
-
 
   const angle = (i/4)*(Math.PI*2);
   sphere.position.x = orbitRadius * Math.cos(angle);
@@ -63,10 +75,47 @@ spheres.rotation.x = 0.1;
 spheres.position.y = -0.8;
 scene.add(spheres);
 
-// Setting up the renderer
-const renderer = new THREE.WebGLRenderer({ canvas : document.querySelector("canvas"), antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+let lastWheelTime = 0;
+const throttleDelay = 2000;
+let scrollCount = 0;
+
+function throttledWheelHandler(event){
+  const currentTime = Date.now();
+  if(currentTime - lastWheelTime >= throttleDelay){
+  
+    lastWheelTime = currentTime;
+    // const direction = event.deltaY > 0 ? "done" : "up";
+
+    scrollCount = (scrollCount + 1) % 4;
+
+    // Animating headings when scrolled
+    const headings = document.querySelectorAll(".heading");
+    gsap.to(headings,{
+      duration:1,
+      y: `-=${100}%`,
+      ease: "power2.inOut",
+    });
+
+    // when scrolled all headings till end reset to initial position
+    if(scrollCount === 0){
+      gsap.to(headings,{
+        duration:1,
+        y: `0`,
+        ease: "power2.inOut",
+      });
+    }
+
+    // Rotating planets 
+    gsap.to(spheres.rotation,{
+      y: `-=${Math.PI/2}`,
+      duration:2,
+      ease: 'expo.easeInOut'
+    })
+
+  }
+}
+
+window.addEventListener("wheel", throttledWheelHandler)
 
 // Rotating up the planets 
 // setInterval(()=>{
@@ -76,6 +125,13 @@ renderer.setPixelRatio(window.devicePixelRatio);
 //     ease: 'expo.easeInOut'
 //   })
 // },2500)
+
+
+// Setting up the renderer
+const renderer = new THREE.WebGLRenderer({ canvas : document.querySelector("canvas"), antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
 
 // Animation loop
 function animate() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          bellina: ['Bellina', 'sans-serif'], // Define the font
+        },
+      },
   },
   plugins: [],
 };
